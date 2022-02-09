e352e89eb08a106a83a4d66e8e65fdcd7e20bb6a
Real Inspiration <ilerwatson22@gmail.com>
2022-02-09 17:06:56 +0000

    Blog model created using class and model object to provide values for what data is required for blogs

---
+++ models/Blog.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Blog',
+  }
+);
+
+module.exports = Blog;
