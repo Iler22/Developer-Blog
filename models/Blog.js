6773d8eed8a65dd4ce87ef92cda26b2ac9535f2b
Real Inspiration <ilerwatson22@gmail.com>
2022-02-12 10:50:43 +0000

    Refactored Blog Model

--- models/Blog.js
+++ models/Blog.js
@@ -1,36 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const connection = require('../config/connection');
 
 class Blog extends Model {}
 
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    contents: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.TEXT,
-      allowNull: false,
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  contents: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id',
     },
   },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'Blog',
-  }
-);
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'blog',
+};
+
+Blog.init(schema, options);
 
 module.exports = Blog;
