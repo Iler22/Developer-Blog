eac1f55127711fea2e8c210a4c14db77d294221e
Real Inspiration <ilerwatson22@gmail.com>
2022-02-12 11:17:06 +0000

    Dummy seeds created for Blogs and Users

---
+++ seeds/blogs.js
@@ -0,0 +1,18 @@
+const blogs = [
+  {
+    title: 'Why MVC is so important',
+    contents:
+      'MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.',
+    username: 'John1234',
+    user_id: 1,
+  },
+  {
+    title: 'Authentication vs Authorization',
+    contents:
+      'There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed to access to the system.',
+    username: 'Jane Smith',
+    user_id: 2,
+  },
+];
+
+module.exports = blogs;
---
+++ seeds/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    username: 'John1234',
+    password: 'password123',
+  },
+  {
+    username: 'Jane Smith',
+    password: 'password456',
+  },
+];
+
+module.exports = users;
