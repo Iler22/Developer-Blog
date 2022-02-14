6488ade5483a475151909d9555e511a0579c7559
Real Inspiration <ilerwatson22@gmail.com>
2022-02-14 19:08:20 +0000

    Removed index js as routes folder now provides routes to the api and views controllers

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const blogs = require('./blogs');
-const users = require('./users');
-
-router.use('/blogs', blogs);
-router.use('/users', users);
-
-module.exports = router;
