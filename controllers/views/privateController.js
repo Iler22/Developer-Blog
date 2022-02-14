2d860717c82b053903f9cab6c090436f04017787
Real Inspiration <ilerwatson22@gmail.com>
2022-02-14 19:25:22 +0000

    Added functionality for a signle blog or the dashboard to be rendered based on a user being logged in.

--- controllers/views/privateController.js
+++ controllers/views/privateController.js
@@ -1,9 +1,42 @@
-const renderSingleBlogPage = (req, res) => {
-  return res.render('singleBlog');
+const { Blog, User } = require('../../models');
+
+const renderSingleBlogPage = async (req, res) => {
+  const { loggedIn, user } = req.session;
+  const { id } = req.params;
+  const blogFromDB = await Blog.findByPk(id, {
+    include: [
+      {
+        model: User,
+        attributes: ['username', 'id'],
+      },
+    ],
+  });
+
+  const blog = blogFromDB.get({ plain: true });
+
+  const isMyBlog = loggedIn && user.id === blog.user.id;
+
+  return res.render('singleBlog', { loggedIn, blog, isMyBlog });
 };
 
-const renderDashboardPage = (req, res) => {
-  return res.render('dashboard');
+const renderDashboardPage = async (req, res) => {
+  const { loggedIn, user } = req.session;
+
+  const blogsFromDB = await Blog.findAll({
+    where: {
+      user_id: req.session.user.id,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  });
+
+  const blogs = blogsFromDB.map((blog) => blog.get({ plain: true }));
+
+  return res.render('dashboard', { loggedIn, blogs, user });
 };
 
 const renderCreateBlogPage = (req, res) => {
