8ddafe796acd7726ba23300ad01fd1713f366793
Real Inspiration <ilerwatson22@gmail.com>
2022-02-16 21:38:12 +0000

    Function created to render user comments on a single blog

--- controllers/views/privateController.js
+++ controllers/views/privateController.js
@@ -1,4 +1,23 @@
-const { Blog, User } = require('../../models');
+const { Blog, User, Comment } = require('../../models');
+
+const renderUserComment = async (req, res) => {
+  const { loggedIn, user } = req.session;
+  const { id } = req.params;
+  const commentFromDB = await Comment.findByPk(id, {
+    include: [
+      {
+        model: User,
+        attributes: ['username', 'id'],
+      },
+    ],
+  });
+
+  const comment = commentFromDB.get({ plain: true });
+
+  const isMyComment = loggedIn && user.id === comment.user.id;
+
+  return res.render('singleBlog', { loggedIn, comment, isMyComment });
+};
 
 const renderSingleBlogPage = async (req, res) => {
   const { loggedIn, user } = req.session;
@@ -47,4 +66,5 @@ module.exports = {
   renderSingleBlogPage,
   renderDashboardPage,
   renderCreateBlogPage,
+  renderUserComment,
 };
