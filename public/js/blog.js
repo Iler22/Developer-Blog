1f0c4fae69cc55c2cae4d9eebe1d73f738c0f088
Real Inspiration <ilerwatson22@gmail.com>
2022-02-16 21:35:16 +0000

    Added jQuery eventlistener to fetch and post date on submission of a users comment.

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,4 +1,5 @@
 const createBlogForm = $('#create-blog-form');
+const commentBtn = $('#comment-btn');
 
 const handleCreateBlog = async (event) => {
   event.preventDefault();
@@ -26,4 +27,29 @@ const handleCreateBlog = async (event) => {
   }
 };
 
+const handleAddComment = async (event) => {
+  event.preventDefault();
+
+  const contents = $('#contents').val();
+
+  const payload = JSON.stringify({
+    contents,
+  });
+
+  const response = await fetch('/api/blogs/:id', {
+    method: 'POST',
+    body: payload,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    window.location.replace('/');
+  } else {
+    alert('Failed to add comment');
+  }
+};
+
+commentBtn.on('click', handleAddComment);
 createBlogForm.on('submit', handleCreateBlog);
