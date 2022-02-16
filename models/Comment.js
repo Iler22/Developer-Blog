1efc43c06af753cfd0c811657b11956fd55774fd
Real Inspiration <ilerwatson22@gmail.com>
2022-02-16 21:36:35 +0000

    Created comment table with parameters and keys

---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const connection = require('../config/connection');
+
+class Comment extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  contents: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'comment',
+};
+
+Comment.init(schema, options);
+
+module.exports = Comment;
