edf0aab02ca8d91a0d02f5312bf737d0c144a665
Real Inspiration <ilerwatson22@gmail.com>
2022-02-16 21:32:58 +0000

    Created new route and function to render comment on single blog page

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -1,15 +1,19 @@
 const { Router } = require('express');
 
+const auth = require('../../middleware/auth');
+
 const {
   createBlog,
   updateBlog,
   deleteBlog,
+  addComment,
 } = require('../../controllers/api/blogs');
 
 const router = Router();
 
-router.post('/', createBlog);
-router.put('/:id', updateBlog);
-router.delete('/:id', deleteBlog);
+router.post('/:id', auth, addComment);
+router.post('/', auth, createBlog);
+router.put('/:id', auth, updateBlog);
+router.delete('/:id', auth, deleteBlog);
 
 module.exports = router;
--- routes/views/privateRoutes.js
+++ routes/views/privateRoutes.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
 const {
-  renderSingleBlogPage,
   renderDashboardPage,
   renderCreateBlogPage,
+  renderSingleBlogPage,
+  renderUserComment,
 } = require('../../controllers/views/privateController');
 
 const router = Router();
 
-router.get('/blog/:id', renderSingleBlogPage);
 router.get('/dashboard', renderDashboardPage);
 router.get('/create-blog', renderCreateBlogPage);
+router.get('/blogs/:id', renderSingleBlogPage);
+router.get('/blogs/:id', renderUserComment);
 
 module.exports = router;
