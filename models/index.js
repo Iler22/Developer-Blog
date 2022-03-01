061b8240f637e9090d0d823c0c26a42618148b21
Real Inspiration <ilerwatson22@gmail.com>
2022-03-01 22:24:39 +0000

    resolved error with model associations

--- models/index.js
+++ models/index.js
@@ -9,14 +9,4 @@ Comment.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-User.hasMany(Blog, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-
 module.exports = { Blog, User, Comment };
